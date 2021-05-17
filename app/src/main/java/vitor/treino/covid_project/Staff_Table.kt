e7a41fb3147f558d21849b5e9dc55e06c1d9741b
segur <seguro17@gmail.com>
2021-05-17 13:17:33 +0100

    Criacao do ficheiro Staff_Table

---
+++ app/src/main/java/vitor/treino/covid_project/Staff_Table.kt
@@ -0,0 +1,13 @@
+package vitor.treino.covid_project
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class Staff_Table(db: SQLiteDatabase) {
+    private val db : SQLiteDatabase = db
+
+    fun create() {
+    }
+}
