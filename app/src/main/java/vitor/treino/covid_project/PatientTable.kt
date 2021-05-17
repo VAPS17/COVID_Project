51dc366e793d99b2f9cac6678cbd2115417546c6
segur <seguro17@gmail.com>
2021-05-17 17:32:47 +0100

    Criacao da funcao create no script PatientTable

--- app/src/main/java/vitor/treino/covid_project/PatientTable.kt
+++ app/src/main/java/vitor/treino/covid_project/PatientTable.kt
@@ -1,4 +1,30 @@
 package vitor.treino.covid_project
 
-class PatientTable {
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class PatientTable(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+
+    fun create() {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" +
+                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                FIELD_IDENTIFICATION + " TEXT NOT NULL UNIQUE," +
+                FIELD_NAME + " TEXT NOT NULL," +
+                FIELD_DISEASE + " TEXT NOT NULL," +
+                FIELD_PRIORITY + " TEXT NOT NULL," +
+                " FOREIGN KEY (" + FIELD_ID_STAFF + ")" +
+                " REFERENCES " + StaffTable.TABLE_NAME + ")")
+    }
+
+    companion object{
+        const val TABLE_NAME = "patient"
+        const val FIELD_IDENTIFICATION = "identification"
+        const val FIELD_NAME = "name"
+        const val FIELD_DISEASE = "disease"
+        const val FIELD_PRIORITY = "priority"
+        const val FIELD_ID_STAFF = "id_staff"
+    }
 }
