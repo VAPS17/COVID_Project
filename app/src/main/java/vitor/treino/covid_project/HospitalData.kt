cfc65df8a23a3acf0677a073677f765a5c29432b
segur <seguro17@gmail.com>
2021-05-27 16:45:43 +0100

    Implementação da função "toContentValues()" no "HospitalData"

--- app/src/main/java/vitor/treino/covid_project/HospitalData.kt
+++ app/src/main/java/vitor/treino/covid_project/HospitalData.kt
@@ -5,4 +5,15 @@ import android.database.Cursor
 import android.provider.BaseColumns
 
 data class HospitalData(var id: Long = -1, var name: String, var location: String, var address: String, var state: String) {
+
+    fun toContentValues(): ContentValues {
+        val values = ContentValues()
+
+        values.put(HospitalTable.FIELD_NAME, name)
+        values.put(HospitalTable.FIELD_LOCATION, location)
+        values.put(HospitalTable.FIELD_ADDRESS, address)
+        values.put(HospitalTable.FIELD_STATE, state)
+
+        return values
+    }
 }
