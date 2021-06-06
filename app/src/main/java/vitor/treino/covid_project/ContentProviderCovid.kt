09ec9890935a4d3799a0f060b9034e2225029f4f
segur <seguro17@gmail.com>
2021-06-06 23:06:59 +0100

    Criação do "ContentProviderCovid" e da função onCreate() para inicializar o Content Provider

---
+++ app/src/main/java/vitor/treino/covid_project/ContentProviderCovid.kt
@@ -0,0 +1,54 @@
+package vitor.treino.covid_project
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns
+
+class ContentProviderCovid : ContentProvider() {
+    private var bdHelper : BDHelper? = null
+
+    override fun onCreate(): Boolean {
+        bdHelper = BDHelper(context)
+
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        val bd = bdHelper!!.readableDatabase
+
+        return when (getUriMatcher().match(uri)) {
+
+        }
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("Implement this to handle requests to delete one or more rows")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO(
+            "Implement this to handle requests for the MIME type of the data" +
+                    "at the given URI"
+        )
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Implement this to handle requests to insert a new row.")
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        TODO("Implement this to handle requests to update one or more rows.")
+    }
+}
