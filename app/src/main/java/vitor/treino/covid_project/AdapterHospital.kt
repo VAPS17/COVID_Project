1be177b769b20c22c18b6571327e659074b0c1d0
segur <seguro17@gmail.com>
2021-06-10 15:49:08 +0100

    Criação do layout item_hospital

--- app/src/main/java/vitor/treino/covid_project/AdapterHospital.kt
+++ app/src/main/java/vitor/treino/covid_project/AdapterHospital.kt
@@ -1,4 +1,33 @@
 package vitor.treino.covid_project
 
-class AdapterHospital {
+import android.database.Cursor
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AdapterHospital(val fragment: HospitalFragment): RecyclerView.Adapter<AdapterHospital.ViewHolderHospital> {
+    public var cursor: Cursor? = null
+        get() = field
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ViewHolderHospital {
+        private val text
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderHospital {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderHospital, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
