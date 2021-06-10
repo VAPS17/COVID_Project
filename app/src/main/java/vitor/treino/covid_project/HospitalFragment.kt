55b508038720b8e098212ac9c5ef25aa095ff7f8
segur <seguro17@gmail.com>
2021-06-11 00:00:46 +0100

    AdapterHospital, HospitalFragment, item_hospital e fragment_hospital criado

--- app/src/main/java/vitor/treino/covid_project/HospitalFragment.kt
+++ app/src/main/java/vitor/treino/covid_project/HospitalFragment.kt
@@ -18,16 +18,61 @@ import vitor.treino.covid_project.databinding.FragmentHospitalBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HospitalFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
+    private var _binding: FragmentHospitalBinding? = null
+    private var adapterHospital : AdapterHospital? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentHospitalBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerViewHospital = view.findViewById<RecyclerView>(R.id.recyclerViewHospital)
+        adapterHospital = AdapterHospital(this)
+        recyclerViewHospital.adapter = adapterHospital
+        recyclerViewHospital.layoutManager = LinearLayoutManager(requireContext())
+
+        LoaderManager.getInstance(this)
+            .initLoader(ID_LOADER_MANAGER_LIVROS, null, this)
+
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
+        return CursorLoader(
+            requireContext(),
+            ContentProviderCovid.ENDERECO_HOSPITAL,
+            HospitalTable.TODAS_COLUNAS,
+            null, null,
+            HospitalTable.FIELD_NAME
+        )
     }
 
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+        adapterHospital!!.cursor = data
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        adapterHospital!!.cursor = null
     }
 
+    companion object {
+        const val ID_LOADER_MANAGER_LIVROS = 0
+    }
 }
