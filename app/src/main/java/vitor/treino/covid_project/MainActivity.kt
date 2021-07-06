4cf944edbfd9cbda0c1dfd2716c600575aa34fd9
segur <seguro17@gmail.com>
2021-07-06 22:22:58 +0100

    Criação do menu_staff

--- app/src/main/java/vitor/treino/covid_project/MainActivity.kt
+++ app/src/main/java/vitor/treino/covid_project/MainActivity.kt
@@ -1,7 +1,6 @@
 package vitor.treino.covid_project
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,7 +8,6 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import vitor.treino.covid_project.databinding.ActivityMainBinding
 
@@ -41,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_hospital, menu)
         this.menu = menu
         return true
     }
